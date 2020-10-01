284d1658b803904627e3f53bc435d7f6cfbf2dfa
student <student@C246U60>
2020-10-01 17:50:19 +0300

    Practice 5

---
+++ Practice6/Pr06-1-1/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Person
+{
+	private string Name;
+	private string Surname;
+	private DateTime Datatime;
+	public string name
+	{
+		get {
+			return this.Name;
+		}
+		set {
+			this.Name=value;
+		}
+	}
+	public string surname
+	{
+		get {
+			return this.Surname;
+		}
+		set {
+			this.Surname = value;
+		}
+	}
+	public DateTime datatime
+	{
+		get {
+			return this.Datatime;
+		}
+		set {
+			this.Datatime = value;
+		}
+	}
+	public int yearDate
+	{
+		get {
+			return this.Datatime.Year;
+		}
+		set {
+			this.Datatime = new DateTime(value, Datatime.Month, Datatime.Day);
+		}
+	}
+	public Person(string Name, string Surname, DateTime Datatime)
+	{
+		name = Name;
+		surname = Surname;
+		datatime = Datatime;
+	}
+	public Person()
+	{
+		name = "";
+		surname = "";
+		datatime = new DateTime(0,0,0);
+	}
+	public override string ToString()
+	{
+		return $"{name} {surname} рожденный {datatime.Day}.{datatime.Month}.{datatime.Year}";
+	}
+	public virtual string ToShortString()
+	{
+		return $"{name} {surname}";
+	}
+
+}
