3bea97f96d5a7e3a472c4e9bd1ef0c21f302c5f6
Maxim Gladyshev <gladyshevma@gmail.com>
2020-10-31 22:15:15 +0300

    Practice9

---
+++ Practice9/PR09-2-1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR09_2_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image MemForImage;
+        private void LoadImage(bool jpg)
+        {
+            openFileDialog1.InitialDirectory = "c:";
+            if (jpg)
+            {
+                openFileDialog1.Filter = "image (JPEG) files (*.jpg)|*.jpg|All files(*.*) | *.* ";
+            }
+            else
+            {
+                openFileDialog1.Filter = "image (PNG) files (*.png)|*.png|All files(*.*) | *.* ";
+            }
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            { 
+                try
+                {
+                    MemForImage = Image.FromFile(openFileDialog1.FileName);
+                    pictureBox1.Image = MemForImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось загрузить файл: " + ex.Message);
+                }
+            }
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult rsl = MessageBox.Show("Уже уходишь?", "Серьезно?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rsl == DialogResult.Yes)
+            { 
+                Application.Exit();
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Form PreView = new Preview(MemForImage);
+            PreView.ShowDialog();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            LoadImage(true);
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            LoadImage(false);
+        }
+
+        private void вФорматеJPGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadImage(true);
+        }
+
+        private void вФорматеPNGToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadImage(false);
+        }
+    }
+}
---
+++ Practice9/PR09-2-1/Preview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR09_2_1
+{
+    public partial class Preview : Form
+    {
+        Image ToView;
+        public Preview()
+        {
+            InitializeComponent();
+        }
+        public Preview(Image view)
+        {
+            ToView = view;
+            InitializeComponent();
+        }
+        private void Preview_Load(object sender, EventArgs e)
+        {
+            if (ToView != null)
+            {
+                pictureBox1.Size = new Size(ToView.Width, ToView.Height);
+                pictureBox1.Image = ToView;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
